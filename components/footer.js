4534ee8f7f1b4d680478afd4f96dfb1f3171128a
Ahnaf-dev <52428475+Ahnaf-dev@users.noreply.github.com>
2022-06-25 16:09:35 -0600

    Typo

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <p className="bold">Build With React & Next JS</p>
+      <p className="bold">Built With React & Next JS</p>
     </footer>
   );
 }
